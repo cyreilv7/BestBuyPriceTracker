e420e8a6b255b236c554f582dff4299fefacb8d7
cyreilv7 <shoiiru3@gmail.com>
2021-12-29 19:58:49 -0800

    Added login and register routes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, flash, request
-from forms import ProductInfoForm, RegistrationForm
+from forms import ProductInfoForm, RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
@@ -11,8 +11,22 @@ def index():
     form = ProductInfoForm()
     if form.validate_on_submit():
         flash('Product is now being tracked!', 'success')
-
     return render_template("index.html", form=form)
+
+@app.route("/register", methods= ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Account has been created!', 'success')
+    return render_template("register.html", form=form)
+
+@app.route("/login", methods= ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login successful', 'success')
+        return redirect("/")    
+    return render_template("login.html", form=form)
 
 if __name__ == "__main__":
   app.run(debug=True,host='0.0.0.0', port=8080)
--- forms.py
+++ forms.py
@@ -1,11 +1,33 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, DecimalField, IntegerField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, URL, NumberRange
+from wtforms.validators import DataRequired, Length, Email, EqualTo, URL, NumberRange, ValidationError
+import re
+
+
+def special_characters(form, field): 
+    characters_regex = re.compile(r'^[a-zA-Z0-9\-_.]+$')
+    mo = characters_regex.search(field.data)
+    if not mo:
+        raise ValidationError('Valid characters are A-Z a-z 0-9 . _ -')
+
 
 class ProductInfoForm(FlaskForm):
     url = StringField('URL', validators=[DataRequired(), URL()])
     price_cutoff = DecimalField('Price Cutoff', places=2, validators=[DataRequired(), NumberRange(min=0)])
     submit = SubmitField('Track Item')
 
+
 class RegistrationForm(FlaskForm):
-    pass
+    username = StringField('Username', validators=[DataRequired(), Length(min=6, max=30), special_characters])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('confirm_password', message='Passwords must match.')])
+    confirm_password = PasswordField('Confirm Password')
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Log In')
+
