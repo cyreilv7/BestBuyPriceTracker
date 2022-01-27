8f43da62f150df28384b3492e56935752185f25f
cyreilv7 <shoiiru3@gmail.com>
2022-01-26 23:30:23 -0800

    Updated 'tracked' route

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,6 @@
 from application import app, db, bcrypt
 from application.models import User, Product, UserProduct
-from application.forms import ProductInfoForm, RegistrationForm, LoginForm
+from application.forms import ProductInfoForm, RegistrationForm, LoginForm, NewPriceCutoffForm
 from flask import render_template, redirect, url_for, flash, request, abort
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -12,12 +12,13 @@ def index():
     form = ProductInfoForm()
     if form.validate_on_submit():
         product = Product.query.filter_by(url=form.url.data).first()
-        relationship = UserProduct.query.filter_by(
+        association = UserProduct.query.filter_by(
             user=current_user, product=product).first()
         if product:
-            if relationship: 
-                flash('You are already tracking this item', 'danger')
+            if association: 
+                flash('You are already tracking this item.', 'danger')
                 return render_template('index.html', form=form)
+        # TODO: Attempt to scrape product info. If fail, flash "There was an error tracking this product."
         else:
             product = Product(name='TestName', price=10.2, url=form.url.data)
             db.session.add(product)
@@ -31,10 +32,11 @@ def index():
         flash('Item is succesfully being tracked!', 'success')
     return render_template('index.html', form=form)
 
-
-@app.route('/currently_tracked', methods=['GET', 'POST'])
+@app.route('/tracked', defaults={'product_id': None}, methods=['GET', 'POST'])
+@app.route('/tracked/<int:product_id>', methods=['GET', 'POST'])
 @login_required
-def tracked():
+def tracked(product_id):
+    form = NewPriceCutoffForm()
     # Fetch all products tracked by the user
     products = db.session.query(Product). \
         join(UserProduct). \
@@ -43,17 +45,25 @@ def tracked():
     product_image_files = [
         url_for('static', filename=f'product_images/{f.image_file}') for f in products]
     product_attributes = zip(products, product_image_files)
-    return render_template('tracked.html', products=products, product_attributes=product_attributes)
+    if form.validate_on_submit():
+        product = Product.query.get(product_id)
+        association = UserProduct.query.filter_by(user=current_user, product=product).first()
+        association.price_cutoff = int(form.price_cutoff.data)
+        db.session.commit()
+        flash('Price cutoff succesfully updated.', 'success')
+    return render_template('tracked.html', products=products, product_attributes=product_attributes, form=form)
 
-@app.route('/stop_tracking<int:product_id>', methods=['GET', 'POST'])
+
+@app.route('/stop_tracking/<int:product_id>', methods=['GET', 'POST'])
 @login_required
 def stop_tracking(product_id):
     product = Product.query.get(product_id)
-    relationship = UserProduct.query.filter_by(user=current_user, product=product).first()
-    if not relationship:
+    association = UserProduct.query.filter_by(user=current_user, product=product).first()
+    # Check if the product (whose id is a query string) is being tracked by the user
+    if not association:
         return redirect(url_for('index'))
     else:
-        db.session.delete(relationship)
+        db.session.delete(association)
         db.session.commit()
     return redirect(url_for('tracked'))
 
@@ -64,6 +74,7 @@ def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     if form.validate_on_submit():
+        # Insert user account info into the database
         username = form.username.data
         email = form.email.data
         hashed_password = bcrypt.generate_password_hash(
@@ -83,15 +94,18 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
+        # Check if username matches password
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember_me.data)
-            # TODO: Figure out what this returns
+            # Parse the query string and go to that route
             next_page = request.args.get('next')
             if next_page:
+                flash('Login was successful!', 'success')
                 return redirect(next_page)
             else:
                 flash('Login was successful!', 'success')
                 return redirect(url_for('index'))
+
         else:
             flash('Login unsuccessful. Please check username or password.', 'danger')
     return render_template('login.html', title='Login', form=form)
