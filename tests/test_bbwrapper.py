a71ed41a8e5ffb3633e381b608fdc5809e523f6c
cyreilv7 <shoiiru3@gmail.com>
2022-05-29 19:24:17 -0700

    update bbwrapper unit tests

--- tests/test_bbwrapper.py
+++ tests/test_bbwrapper.py
@@ -11,21 +11,26 @@ TEST_URL = "https://www.bestbuy.com/site/apple-iphone-13-pro-max-5g-128gb-alpine
 test_product = ProductInfo(sku=TEST_SKU)
 
 
-def test_primary_info(product_attributes):
-    # test constructor
+def test_constructor():
     assert (test_product.sku)
 
+    # test if sku is populated if only url is provided 
+    test_product_2 = ProductInfo(url=TEST_URL)
+    assert (test_product_2.sku)
+
+
+def test_primary_info(product_attributes):
     # test setters
     test_product.set_primary_info()
     assert set(test_product.__dict__.keys()).issubset(product_attributes)
-    assert (test_product.page_url)
-
+    
 
 def test_get_product_sku():
     valid_url = TEST_URL
     invalid_url = "https://www.bestbuy.com"
 
     assert (test_product.get_product_sku(valid_url) == TEST_SKU)
+
     with pytest.raises(ValueError):
         test_product.get_product_sku(invalid_url)
 
@@ -36,7 +41,9 @@ def test_save_img():
     parent_folder = os.path.join(script_dir, "application/static/product_images")
 
     assert (test_product.image_filename)
-    assert (os.path.isfile(f"{parent_folder}/{test_product.image_filename}"))
+    
+    # test if product image saved to correct folder
+    assert (os.path.isfile(f"{parent_folder}/{test_product.image_filename}")) 
 
 
 @pytest.mark.skip(reason="debating if this should be a method")
